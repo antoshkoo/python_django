3714b1c0e2dd16d3104e860359c465f290f15ba6
lilu <antoshkoo@gmail.com>
2020-11-23 13:14:01 +0300

    DRY added

--- 01_IntroductionToWebFrameworks/todo/tasks/views.py
+++ 01_IntroductionToWebFrameworks/todo/tasks/views.py
@@ -10,11 +10,12 @@ class ToDoView(View):
 
         todo_list = ['Установить python', 'Установить django', 'Запустить сервер', 'Порадоваться результату',
                      'Внимательно прочитать ДЗ', 'Подумать', 'Сделать красиво', 'Сдать домашку:)', 'Порадоваться']
+        # Я бы передал итерируемый объект если б не <ul></ul>, c ними нашел как сделать с помощью стриминга.
+        # Но это мне показалось слишком сложным вариантом для такой задачи:)
+        responce = HttpResponse()
+        responce.write('<ul>')
+        for i in range(5):
+            responce.write(f'<li>{random.choice(todo_list)}</li>')
+        responce.write('</ul>')
+        return responce
 
-        return HttpResponse(f'<ul>'
-                            f'<li>{random.choice(todo_list)}</li>'
-                            f'<li>{random.choice(todo_list)}</li>'
-                            f'<li>{random.choice(todo_list)}</li>'
-                            f'<li>{random.choice(todo_list)}</li>'
-                            f'<li>{random.choice(todo_list)}</li>'
-                            f'</ul>')
