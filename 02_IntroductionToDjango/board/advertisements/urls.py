eed3275711f2202a41fc757d4ac81f6dbb2aaaf1
lilu <antoshkoo@gmail.com>
2020-11-24 15:02:05 +0300

    Names fixed

--- 02_IntroductionToDjango/board/advertisements/urls.py
+++ 02_IntroductionToDjango/board/advertisements/urls.py
@@ -3,9 +3,9 @@ from .import views
 
 urlpatterns = [
    path('', views.advertisements_list, name='advertisements_list'),
-   path('advertisements/1/', views.advertisement_1, name='advertisements_detail'),
-   path('advertisements/2/', views.advertisement_2, name='advertisements_detail'),
-   path('advertisements/3/', views.advertisement_3, name='advertisements_detail'),
-   path('advertisements/4/', views.advertisement_4, name='advertisements_detail'),
-   path('advertisements/5/', views.advertisement_5, name='advertisements_detail'),
+   path('advertisements/1/', views.advertisement_1, name='advertisements_detail_1'),
+   path('advertisements/2/', views.advertisement_2, name='advertisements_detail_2'),
+   path('advertisements/3/', views.advertisement_3, name='advertisements_detail_3'),
+   path('advertisements/4/', views.advertisement_4, name='advertisements_detail_4'),
+   path('advertisements/5/', views.advertisement_5, name='advertisements_detail_5'),
 ]
