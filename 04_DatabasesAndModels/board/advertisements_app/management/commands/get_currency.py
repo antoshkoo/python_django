86b6239b06a202d31a71bdb0972f503be34ae713
lilu <antoshkoo@gmail.com>
2020-12-17 13:24:07 +0300

    get_currency fix

--- 04_DatabasesAndModels/board/advertisements_app/management/commands/get_currency.py
+++ 04_DatabasesAndModels/board/advertisements_app/management/commands/get_currency.py
@@ -1,6 +1,7 @@
 import requests
 from django.core.management.base import BaseCommand
 from django.utils import timezone
+import datetime
 
 from advertisements_app.models import AdvertisementCurrency
 
@@ -9,15 +10,19 @@ class Command(BaseCommand):
     help = 'Получение курса валют'
 
     def handle(self, *args, **kwargs):
-        responce = requests.get('https://www.cbr-xml-daily.ru/daily_json.js')
-        if responce.status_code == 200:
-            data = responce.json()
-            usd_currency = data['Valute']['USD']['Value']
-            obj, created = AdvertisementCurrency.objects.get_or_create(
-                date=timezone.now(),
-                defaults={'currency_name': 'USD',
-                          'currency': usd_currency},
-            )
-            return f'Курс обновлен'
+        today = timezone.now().strftime('%Y-%m-%d')
+        if AdvertisementCurrency.objects.filter(date=today).exists():
+            return f'Курс был обновлен {today}'
         else:
-            return f'Ошибка парсинга'
+            responce = requests.get('https://www.cbr-xml-daily.ru/daily_json.js')
+            if responce.status_code == 200:
+                data = responce.json()
+                usd_currency = data['Valute']['USD']['Value']
+                AdvertisementCurrency.objects.create(
+                    date=timezone.now(),
+                    currency_name='USD',
+                    currency=usd_currency,
+                )
+                return f'Курс обновлен {today}'
+            else:
+                return f'Ошибка парсинга'
