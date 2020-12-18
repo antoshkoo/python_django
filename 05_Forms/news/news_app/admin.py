7161f7fe728fc5e640e113e6e7347d028bdbcff2
lilu <antoshkoo@gmail.com>
2020-12-18 16:42:32 +0300

    admin extra

--- 05_Forms/news/news_app/admin.py
+++ 05_Forms/news/news_app/admin.py
@@ -1,8 +1,12 @@
 from django.contrib import admin
+from django.contrib.auth.models import Group
+
 from .models import *
 
 # Register your models here.
 
+admin.site.site_header = 'News Admin'
+admin.site.unregister(Group)
 
 class NewsCommentsInLine(admin.TabularInline):
     model = NewsComments
@@ -10,19 +14,24 @@ class NewsCommentsInLine(admin.TabularInline):
 
 @admin.register(News)
 class NewsAdmin(admin.ModelAdmin):
-    list_display = ['name', 'created_at', 'updated_at', 'is_active']
+    list_display = ['id', 'name', 'created_at', 'updated_at', 'is_active']
+    list_display_links = ['id', 'name']
+    list_editable = ['is_active']
     list_filter = ['is_active']
     inlines = [NewsCommentsInLine]
+    fields = ['name', 'body', ('created_at', 'updated_at'), 'is_active']
+    readonly_fields = ['created_at', 'updated_at']
+    save_on_top = True
+
 
     actions = ['make_active', 'make_inactive']
 
     def make_active(self, request, queryset):
         queryset.update(is_active=1)
+    make_active.short_description = 'Сделать активными'
 
     def make_inactive(self, request, queryset):
         queryset.update(is_active=0)
-
-    make_active.short_description = 'Сделать активными'
     make_inactive.short_description = 'Сделать неактивными'
 
 
@@ -37,7 +46,7 @@ class NewsCommentsAdmin(admin.ModelAdmin):
         queryset.update(comment_text='Удалено администратором')
 
     def short_comment(self, obj):
-        if len(obj.comment_text) < 15 or obj.comment_text=='Удалено администратором':
+        if len(obj.comment_text) < 15 or obj.comment_text == 'Удалено администратором':
             return obj.comment_text
         else:
             return f'{obj.comment_text[:15]}...'
