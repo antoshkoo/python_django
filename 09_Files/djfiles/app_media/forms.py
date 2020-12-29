b9432f56b127e50df76cfeb83f0295fd099a0d8c
lilu <antoshkoo@gmail.com>
2020-12-29 16:57:38 +0300

    Lesson 9. practice fix.

--- 09_Files/djfiles/app_media/forms.py
+++ 09_Files/djfiles/app_media/forms.py
@@ -1,7 +1,8 @@
 from django import forms
+from .models import File
 
 
-class UploadFileForm(forms.Form):
-    title = forms.CharField(max_length=100)
-    file = forms.FileField()
-
+class UploadDocumentForm(forms.ModelForm):
+    class Meta:
+        model = File
+        fields = ['title', 'file']
