9186e21fa6a0e11a7d484fcd9ef5ede6c4df70c5
lilu <antoshkoo@gmail.com>
2021-03-01 01:25:15 +0300

    homework 13 Pagination complete

--- 13_IntroductionToDjangoRESTFramework/djlibrary/app_api/v1/catalog/api.py
+++ 13_IntroductionToDjangoRESTFramework/djlibrary/app_api/v1/catalog/api.py
@@ -1,4 +1,5 @@
 from rest_framework import viewsets, filters
+from rest_framework.pagination import PageNumberPagination
 from rest_framework.permissions import IsAdminUser
 
 from app_api.v1.catalog.permissions import ReadOnly
@@ -6,23 +7,31 @@ from app_api.v1.catalog.serializers import AuthorSerializer, BookSerializer
 from app_catalog.models import Author, Book
 
 
+class LimitPagination(PageNumberPagination):
+    page_size = 2
+    page_size_query_param = 'page_size'
+    max_page_size = 1000
+
+
 class AuthorViewSet(viewsets.ModelViewSet):
-    queryset = Author.objects.all()
+    queryset = Author.objects.all().order_by('pk')
     serializer_class = AuthorSerializer
+    pagination_class = LimitPagination
     permission_classes = [IsAdminUser | ReadOnly]
     filter_backends = [filters.SearchFilter]
     search_fields = ['first_name', 'last_name']
 
 
 class BookViewSet(viewsets.ModelViewSet):
-    queryset = Book.objects.all()
+    queryset = Book.objects.all().order_by('pk')
     serializer_class = BookSerializer
+    pagination_class = LimitPagination
     permission_classes = [IsAdminUser | ReadOnly]
     filter_backends = [filters.SearchFilter]
     search_fields = ['title']
 
     def get_queryset(self):
-        queryset = Book.objects.all()
+        queryset = Book.objects.all().order_by('pk')
         filter_author = self.request.query_params.get('author')
         filter_pages = self.request.query_params.get('pages')
         filter_pages_params = self.request.query_params.get('filter')
